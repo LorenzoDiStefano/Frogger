67b2b0b4daa053bff74cf7c52a94889466eb9f90
LorenzoDiStefano <lorenzodistefano.k@gmail.com>
2019-12-05 15:54:19 +0100

    Add new vector2 functions

--- engine/vector2.c
+++ engine/vector2.c
@@ -50,36 +50,71 @@ vector2_t vector2_sub(vector2_t *first_value,vector2_t *second_value)
     return result;
 }
 
+vector2_t vector2_mul(vector2_t *first_value, double value)
+{
+    vector2_t result;
+
+    result.x = first_value->x*value;
+    result.y = first_value->y*value;
+
+    return result;
+}
+vector2_t vector2_get_deep_copy(vector2_t *vector2)
+{
+    vector2_t result;
+
+    result.x = vector2->x;
+    result.y = vector2->y;
+
+    return result;
+}
+
 #ifdef _TEST
 
 static int test_init(vector2_t *vector2)
 {
     int ret_val = vector2_init(vector2);
-    return vector2->x==0 && vector2->y==0 && ret_val == 1;
+    return vector2->x == 0 && vector2->y == 0 && ret_val == 1;
 }
 
 static int test_init_safe(vector2_t *vector2)
 {
-    int ret_val = vector2_init_safe(vector2,50,70);
-    return vector2->x==50 && vector2->y==70 && ret_val == 1;
+    int ret_val = vector2_init_safe(vector2, 50, 70);
+    return vector2->x == 50 && vector2->y == 70 && ret_val == 1;
 }
 
 static int test_add(vector2_t *vector2)
 {
     vector2_t second_vector2;
-    vector2_init_safe(vector2,50,70);
-    vector2_init_safe(&second_vector2,50,70);
-    second_vector2= vector2_add(vector2, &second_vector2);
-    return second_vector2.x==100 && second_vector2.y==140;
+    vector2_init_safe(vector2, 50, 70);
+    vector2_init_safe(&second_vector2, 50, 70);
+    second_vector2 = vector2_add(vector2, &second_vector2);
+    return second_vector2.x == 100 && second_vector2.y == 140;
 }
 
 static int test_sub(vector2_t *vector2)
 {
     vector2_t second_vector2;
-    vector2_init_safe(vector2,50,70);
-    vector2_init_safe(&second_vector2,50,70);
+    vector2_init_safe(vector2, 50, 70);
+    vector2_init_safe(&second_vector2, 50, 70);
     second_vector2= vector2_sub(vector2, &second_vector2);
-    return second_vector2.x==0 && second_vector2.y==0;
+    return second_vector2.x == 0 && second_vector2.y == 0;
+}
+
+static int test_deep_copy(vector2_t *vector2)
+{
+    vector2_init_safe(vector2, 50, 70);
+    vector2_t second_vector2 = vector2_get_deep_copy(vector2);
+    return second_vector2.x == 50 && second_vector2.y==70 &&
+    &(second_vector2.y) != &(vector2->y) && &(second_vector2.x) != &(vector2->x);
+}
+
+static int test_mul(vector2_t *vector2)
+{
+    vector2_t second_vector2;
+    vector2_init_safe(vector2, 10, 2);
+    second_vector2= vector2_mul(vector2, 4);
+    return second_vector2.x == 40 && second_vector2.y == 8;
 }
 
 void test_vector2()
@@ -88,5 +123,7 @@ void test_vector2()
     RUN_TEST_VECTOR2(test_init_safe);
     RUN_TEST_VECTOR2(test_add);
     RUN_TEST_VECTOR2(test_sub);
+    RUN_TEST_VECTOR2(test_mul);
+    RUN_TEST_VECTOR2(test_deep_copy);
 }
 #endif
--- engine/vector2.h
+++ engine/vector2.h
@@ -32,9 +32,12 @@ typedef struct vector2
 
 }vector2_t;
 
-vector2_t vector2_add(vector2_t *first_value,vector2_t *second_value);//return first_value*second_value
-vector2_t vector2_sub(vector2_t *first_value,vector2_t *second_value);//return first_value-second_value
+vector2_t vector2_add(vector2_t *first_value, vector2_t *second_value);//return first_value+second_value
+vector2_t vector2_sub(vector2_t *first_value, vector2_t *second_value);//return first_value-second_value
+vector2_t vector2_mul(vector2_t *first_value, double value);
+vector2_t vector2_get_deep_copy(vector2_t *vector2);
 int vector2_init(vector2_t *vector2);
+int vector2_init_safe(vector2_t *vector2, float x, float y);
 
 
 #ifdef _TEST
